ba61ffb8799104f349d3b157a2877b350f6f66cf
Victor Augusto <victorservo@icloud.com>
2026-04-22 03:04:03 -0300

    ajustando conexao mongo com fallback

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -12,25 +13,29 @@ app.use((req, res, next) => {
   );
   next();
 });
+
 app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
+
 const routes = require("./routes/routes");
 app.use("/api", routes);
 
-app.listen(PORT, () => {
-  console.log(`Server Started at ${PORT}`);
-});
-// Obtendo os parametros passados pela linha de comando
-var userArgs = process.argv.slice(2);
-var mongoURL = userArgs[0];
-//Configurando a conexao com o Banco de Dados
 var mongoose = require("mongoose");
-mongoose.connect(mongoURL);
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://Victor200551:528317@cluster0.37l2nej.mongodb.net/?appName=Cluster0",
+);
 mongoose.Promise = global.Promise;
+
 const db = mongoose.connection;
 db.on("error", (error) => {
   console.log(error);
 });
 db.once("connected", () => {
   console.log("Database Connected");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Started at ${PORT}`);
 });
