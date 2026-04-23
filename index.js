4a7a52bd8ea45fbb2385dd8f32ba0bba9b37d302
MiguelGollega <gollegamiguel9@gmail.com>
2026-04-22 21:23:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use("/api", routes);
 var mongoose = require("mongoose");
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://Victor200551:528317@cluster0.37l2nej.mongodb.net/?appName=Cluster0",
+    "mongodb+srv://<db_username>:254140@bancodados.uovh3ct.mongodb.net/?appName=BancoDados",
 );
 mongoose.Promise = global.Promise;
 
